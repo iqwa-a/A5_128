f787784398c5a4074f6233e400ecf7990226ae9a
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-23 22:32:00 +0700

    menambahkan val jenishewan dan dokter

--- app/src/main/java/com/example/rumahhewan/repository/HewanContainer.kt
+++ app/src/main/java/com/example/rumahhewan/repository/HewanContainer.kt
@@ -1,6 +1,8 @@
 package com.example.rumahhewan.repository
 
+import com.example.rumahhewan.Service.DokterService
 import com.example.rumahhewan.Service.HewanService
+import com.example.rumahhewan.Service.JenisHewanService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -8,10 +10,13 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val hewanRepository: HewanRepository
+    val jenishewanRepository: JenisHewanRepository
+    val dokterRepository: DokterRepository
 }
 
 class HewanContainer: AppContainer {
-    private val baseUrl = "http://192.168.100.67/pama5/"
+    private val baseUrl = "http://10.0.2.2/pama5/"
+
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
@@ -21,4 +26,12 @@ class HewanContainer: AppContainer {
     override val hewanRepository: HewanRepository by lazy {
         NetworkHewanRepository(hewanService)
     }
+
+    private val jenishewanService: JenisHewanService by lazy { retrofit.create(JenisHewanService::class.java) }
+    override val jenishewanRepository: JenisHewanRepository by lazy {
+        NetworkJenisHewanRepository(jenishewanService)}
+
+    private val dokterService: DokterService by lazy { retrofit.create(DokterService::class.java) }
+    override val dokterRepository: DokterRepository by lazy {
+        NetworkDokterRepository(dokterService)}
 }
