25e51c0b0b95271634b3cfaefb836d2185b20751
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-26 02:39:40 +0700

    Membuat JenisApp

---
+++ app/src/main/java/com/example/rumahhewan/ui/viewmodel/jenishewan/JenisApp.kt
@@ -0,0 +1,29 @@
+package com.example.rumahhewan.ui.viewmodel.jenishewan
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.rumahhewan.ui.navigasi.PengelolaHalamanHewan
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun JenisApp(){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalamanHewan()
+        }
+    }
+}
