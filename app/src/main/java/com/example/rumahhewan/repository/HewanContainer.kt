c0633e9d8340c18e3c1135038fca002f2da0da23
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-01-27 15:43:21 +0700

    Menmbahkan Container PerawatanRepository

--- app/src/main/java/com/example/rumahhewan/repository/HewanContainer.kt
+++ app/src/main/java/com/example/rumahhewan/repository/HewanContainer.kt
@@ -3,6 +3,7 @@ package com.example.rumahhewan.repository
 import com.example.rumahhewan.Service.DokterService
 import com.example.rumahhewan.Service.HewanService
 import com.example.rumahhewan.Service.JenisHewanService
+import com.example.rumahhewan.Service.PerawatanService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -12,9 +13,10 @@ interface AppContainer {
     val hewanRepository: HewanRepository
     val jenishewanRepository: JenisHewanRepository
     val dokterRepository: DokterRepository
+    val perawatanRepository: PerawatanRepository // Nama property seharusnya perawatanRepository bukan perawatanService
 }
 
-class HewanContainer: AppContainer {
+class HewanContainer : AppContainer {
     private val baseUrl = "http://10.0.2.2/pama5/"
 
     private val json = Json { ignoreUnknownKeys = true }
@@ -29,9 +31,16 @@ class HewanContainer: AppContainer {
 
     private val jenishewanService: JenisHewanService by lazy { retrofit.create(JenisHewanService::class.java) }
     override val jenishewanRepository: JenisHewanRepository by lazy {
-        NetworkJenisHewanRepository(jenishewanService)}
+        NetworkJenisHewanRepository(jenishewanService)
+    }
 
     private val dokterService: DokterService by lazy { retrofit.create(DokterService::class.java) }
     override val dokterRepository: DokterRepository by lazy {
-        NetworkDokterRepository(dokterService)}
+        NetworkDokterRepository(dokterService)
+    }
+
+    private val perawatanService: PerawatanService by lazy { retrofit.create(PerawatanService::class.java) }
+    override val perawatanRepository: PerawatanRepository by lazy {
+        NetworkPerawatanRepository(perawatanService)
+    }
 }
